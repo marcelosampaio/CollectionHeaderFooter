50ee45fe12e7affdd0d9a675b0ad8a85c82e741d
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2020-04-02 17:44:58 -0300

    Initial Commit

---
+++ CollectionHeaderFooter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionHeaderFooter
+//
+//  Created by Marcelo Sampaio on 02/04/20.
+//  Copyright © 2020 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
